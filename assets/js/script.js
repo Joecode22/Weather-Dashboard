01bf68fcaf82fcb42998c9463fad4e4f5eb3ec83
josik <joe.code.mckinney@gmail.com>
2023-04-15 20:49:38 -0500

    added event listener to the search button and created the function that can grab the text input as a string

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,13 @@ var APIKey = "0a4b94a5f4f88fe22b1db9a21f0ba2cc";
 var city;
 currentForcast = `api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}`;
 
+// Function to change the content of t2
+function grabSearch() {
+  const searchText = document.getElementById("search-text");
+  return searchText.value
+}
+
+// Add event listener to table
+const searchBtn = document.getElementById("search-submit");
+searchBtn.addEventListener("click", grabSearch);
+
