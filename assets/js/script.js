bd44cf7612afe0943675d2baac61d8f6010f7fc5
josik <joe.code.mckinney@gmail.com>
2023-04-15 21:31:16 -0500

    completed button generator base level functionality, now need to go back and add input validation and behavior control

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,21 @@ currentForcast = `api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey
 const searchBtn = document.getElementById("search-submit");
 searchBtn.addEventListener("click", grabSearch);
 
-// Function to change the content of t2
+// Function to grab the search term when the search button is clicked
 function grabSearch() {
   const searchText = document.getElementById("search-text");
   const cityName = searchText.value;
   mkCityBtn(cityName);
-}
+};
 
 //function that makes city history buttons
+//Also appends them to the aside
 function mkCityBtn(cityName) {
   const cityBtn = document.createElement('button');
+  const aside = document.getElementById('aside');
   cityBtn.id = cityName;
   cityBtn.type = "submit";
   cityBtn.classList.add('fs-4', 'ms-1', 'btn-primary', 'mb-3', 'w-75', 'p-2');
   cityBtn.textContent = cityName;
-  // console.log('cityBtn: ' + cityBtn)
-  // appendHTML();
-}
-
-//function that appends html to the page
-// function appendHTML() {
-
-// }
+  aside.appendChild(cityBtn);
+};
