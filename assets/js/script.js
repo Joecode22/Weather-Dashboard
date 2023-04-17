19203392eeaead3785abcdd29a6f131953ad3291
josik <joe.code.mckinney@gmail.com>
2023-04-16 23:11:54 -0500

    I was able to generate the cards but i need to position and style them correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,11 @@ document.addEventListener("DOMContentLoaded", function() {
   //Conversions
   //Kelvin to Fahrenheit
   function kelvinToFahrenheit(kelvin) {
-    return (kelvin - 273.15) * 9/5 + 32;
+    return Math.floor((kelvin - 273.15) * 9/5 + 32);
   }
   //Kelvin to Celsius
   function kelvinToCelsius(kelvinTemp) {
-    return kelvinTemp - 273.15;
+    return Math.floor(kelvinTemp - 273.15);
   }
   
 
@@ -92,7 +92,7 @@ document.addEventListener("DOMContentLoaded", function() {
       latitude = data.coord.lat;
       longitude = data.coord.lon;
       date = new Date(data.dt * 1000).toLocaleDateString();
-      temperature = Math.floor(kelvinToCelsius(data.main.temp));
+      temperature = kelvinToCelsius(data.main.temp);
       wind = data.wind.speed;
       humidity = data.main.humidity;
       iconCode = data.weather[0].icon;
@@ -115,6 +115,58 @@ document.addEventListener("DOMContentLoaded", function() {
       }
       const data = await response.json();
       console.log(data);
+      // Loop through the data and create a card for each day
+      for (let i = 0; i < data.list.length; i += 8) {
+        const day = data.list[i];
+      
+        // Create the card element
+        const card = document.createElement('div');
+        card.classList.add('card');
+      
+        // Create the card body element
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+      
+        // Create the date element
+        const date = document.createElement('h5');
+        date.classList.add('card-title');
+        date.textContent = new Date(day.dt * 1000).toLocaleDateString();
+      
+        // Create the weather icon element
+        const icon = document.createElement('img');
+        icon.classList.add('card-img-top');
+        icon.src = `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+      
+        // Create the temperature element
+        const temp = document.createElement('p');
+        temp.classList.add('card-text');
+        temp.textContent = `Temperature: ${kelvinToFahrenheit(day.main.temp)}°F`;
+      
+        // Create the wind element
+        const wind = document.createElement('p');
+        wind.classList.add('card-text');
+        wind.textContent = `Wind: ${day.wind.speed} MPH`;
+      
+        // Create the humidity element
+        const humidity = document.createElement('p');
+        humidity.classList.add('card-text');
+        humidity.textContent = `Humidity: ${day.main.humidity}%`;
+      
+        // Append the elements to the card body
+        cardBody.appendChild(date);
+        cardBody.appendChild(icon);
+        cardBody.appendChild(temp);
+        cardBody.appendChild(wind);
+        cardBody.appendChild(humidity);
+      
+        // Append the card body to the card
+        card.appendChild(cardBody);
+      
+        // Append the card to the container element
+        const container = document.getElementById('container');
+        container.appendChild(card);
+      }
+        
     } catch (error) {
       console.error(`Error fetching five-day forecast: ${error.message}`);
     }
@@ -137,59 +189,5 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("temperature").textContent = `Temp: ${temperature}°F`;
     document.getElementById("wind").textContent = `Wind: ${wind} MPH`;
     document.getElementById("humidity").textContent = `Humidity: ${humidity}%`;
-  }
-
-  // Loop through the data and create a card for each day
-  for (let i = 0; i < data.list.length; i += 8) {
-    const day = data.list[i];
-
-    // Create the card element
-    const card = document.createElement('div');
-    card.classList.add('card');
-
-    // Create the card body element
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-
-    // Create the date element
-    const date = document.createElement('h5');
-    date.classList.add('card-title');
-    date.textContent = new Date(day.dt * 1000).toLocaleDateString();
-
-    // Create the weather icon element
-    const icon = document.createElement('img');
-    icon.classList.add('card-img-top');
-    icon.src = `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
-
-    // Create the temperature element
-    const temp = document.createElement('p');
-    temp.classList.add('card-text');
-    temp.textContent = `Temperature: ${kelvinToFahrenheit(day.main.temp)}°F`;
-
-    // Create the wind element
-    const wind = document.createElement('p');
-    wind.classList.add('card-text');
-    wind.textContent = `Wind: ${day.wind.speed} MPH`;
-
-    // Create the humidity element
-    const humidity = document.createElement('p');
-    humidity.classList.add('card-text');
-    humidity.textContent = `Humidity: ${day.main.humidity}%`;
-
-    // Append the elements to the card body
-    cardBody.appendChild(date);
-    cardBody.appendChild(icon);
-    cardBody.appendChild(temp);
-    cardBody.appendChild(wind);
-    cardBody.appendChild(humidity);
-
-    // Append the card body to the card
-    card.appendChild(cardBody);
-
-    // Append the card to the container element
-    const container = document.getElementById('container');
-    container.appendChild(card);
-}
-
-  
+  };
 });
